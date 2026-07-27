b044187265faf17e79e388938e4362b18c798fa2
KamranYusupov <kamrankek48@gmail.com>
2026-07-27 19:34:16 +0400

    reloader.py fix

--- bot/reloader.py
+++ bot/reloader.py
@@ -17,30 +17,30 @@ class ChangeHandler(FileSystemEventHandler):
 
     def __init__(self, script_name) -> None:
         self.script_name = script_name
-        self.process = subprocess.Popen(["python3", self.script_name])
+        self.process = subprocess.Popen(['python3', self.script_name])
 
     def on_any_event(self, event):
         if event.is_directory:
             return None
 
-        if "__pycache__" in Path(event.src_path).parts:
+        if '__pycache__' in Path(event.src_path).parts:
             return None
 
         if isinstance(event, (FileCreatedEvent, FileModifiedEvent, FileDeletedEvent)):
             logger.info(
-                f"Detected change in: {event.src_path}. Change type: {event.event_type}"
+                f'Detected change in: {event.src_path}. Change type: {event.event_type}'
             )
             self.restart_script()
 
     def restart_script(self):
-        logger.info("Reloading bot...")
+        logger.info('Reloading bot...')
         self.process.kill()
-        self.process = subprocess.Popen(["python3", self.script_name])
+        self.process = subprocess.Popen(['python3', self.script_name])
 
 
-if __name__ == "__main__":
-    app_dir = Path("bot/")
-    script_path = app_dir / "main.py"
+if __name__ == '__main__':
+    app_dir = Path('.')
+    script_path = 'bot/main.py'
 
     event_handler = ChangeHandler(script_path)
     observer = Observer()
