b52307d0a9da208915de93e82386310ae541f09f
KamranYusupov <kamrankek48@gmail.com>
2026-07-31 11:47:13 +0400

    gunicorn conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+set -e
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-gunicorn web.core.wsgi:application --bind 0.0.0.0:8000 --log-level warning
+gunicorn web.core.wsgi:application -c gunicorn.conf.py
---
+++ gunicorn.conf.py
@@ -0,0 +1,15 @@
+bind = "0.0.0.0:8000"
+
+workers = 3
+
+timeout = 60
+
+graceful_timeout = 30
+
+max_requests = 1000
+max_requests_jitter = 100
+
+loglevel = "warning"
+
+accesslog = "-"
+errorlog = "-"
